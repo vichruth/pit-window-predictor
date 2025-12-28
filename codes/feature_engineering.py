0470af574f0b90b53a5170ea86990313f4501c65
Vichruth M <vichruth.victorious@gmail.com>
2025-12-28 15:13:18 +0530

    updated with no mistakes

--- codes/feature_engineering.py
+++ codes/feature_engineering.py
@@ -2,19 +2,19 @@ import pandas as pd
 import numpy as np
 
 # load the dataset to be used for feature engineering
-data = pd.read_csv('data/dataset.csv')
+df= pd.read_csv('/home/vichruth/ml_projects/pit-window-predictor/f1_dataset/processed_laps_2018_2024.csv')
 df['is_pit']= df['pit_stop_number']>0
 print("Pit stop flagged")
 
 # stint logic 
-df['stint_marker']=df.groupby(['race_id','driver_id'])['is_pit'].shift(1).fillna(False)
+df['stint_marker']=df.groupby(['raceId','driverId'])['is_pit'].shift(1).fillna(False)
 print("Stint calculated")
 df['stint_marker']=df['stint_marker'].astype(int)
-df['stint_id']=df.groupby(['race_id','driver_id'])['stint_marker'].cumsum()+1
+df['stint_id']=df.groupby(['raceId','driverId'])['stint_marker'].cumsum()+1
 print("Stint ID ASSIGNED")
 
 #calculate tire age and save the data
-df['tire_age']=df.groupby(['race_id','driver_id','stint_id']).cumcount()+1
+df['tire_age']=df.groupby(['raceId','driverId','stint_id']).cumcount()+1
 print("Tire age calculated")
-df.to_csv('f1_dataset/f1_feature_engineered_data.csv', index=False)
+df.to_csv('../f1_dataset/f1_feature_engineered_data.csv', index=False)
 print("everything completed")
